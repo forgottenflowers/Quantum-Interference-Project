b3236d621480b056129bb5473e35ee753a9a7da5
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-03-09 12:20:03 +0100

    Update PointAntenna_SingleSlot_Radiation.py

--- PointAntenna_SingleSlot_Radiation.py
+++ PointAntenna_SingleSlot_Radiation.py
@@ -1,27 +1,29 @@
-# Generate radiation pattern, when point antenna radiation is blocked by a screen with a single slit.
+# Generate radiation pattern, when point antenna radiation is blocked by a screen with a single slot.
 
 import math
 
 def run():
 	lamda=5.0       			            # Wavelength
-	l=30					                    # length of slit (even)
-	b=12					                    # breadth of slit (even)
-	D=10000.0				                  # distance of detection from screen
+	l=30					            # length of slit (even)
+	b=12					            # breadth of slit (even)
+	D=10000.0				            # distance of detection from screen
 	F=(b*b)/(D*lamda)
 	print "Fresnel number = ",F,":"
 	I0=10.0         			            # Intensity amplitude
 	A=math.sqrt(I0)
-	p=1000       				              # number of data points
+	p=1000       				            # number of data points
 	pi=3.1416
-  size=100
-	ant=500					                  # distance of antenna from screen
+	size=1000000					    # size of open space we are calculating for (you can make this number smaller)
+	ant=500					            # distance of antenna from screen
 
 
 	for np in range(-p,p+1):
 		RPAT=0
 		IPAT=0
-		for nl in range(-l/2,l/2+1):
-			for nb in range(-b/2,b/2+1):
+		for nl in range(-size,size+1):
+			for nb in range(-size,size+1):
+				if (nl>-l/2) & (nl<l/2) & (nb>-b/2) & (nb<b/2):
+					continue		
 				y=np-nb                              #vertical displacement y			
 				temp=nl*nl+D*D                       #x^2+z^2
 				add=math.sqrt(nb*nb+nl*nl+ant*ant)   #antenna to screen
@@ -31,26 +33,6 @@ def run():
 				R=A*math.cos(2*pi*t/lamda)/q         #real
 				I=A*math.sin(2*pi*t/lamda)/q         #imaginary	
 				RPAT=RPAT+R                          #superposition
-				IPAT=IPAT+I
-		print RPAT*RPAT+IPAT*IPAT
-
-
-	for np in range(-100,161):
-		RPAT=0
-		IPAT=0
-		for nl in range(-size,size+1):
-			for nb in range(-size,size+1):
-				if (nl>-l/2) & (nl<l/2) & (nb>-b/2) & (nb<b/2):
-					continue					
-				y=np-nb			
-				temp=nl*nl+D*D
-				add=math.sqrt(nb*nb+nl*nl+ant*ant)
-				root=math.sqrt(y*y+temp)
-				t=root+add
-				q=root*add				
-				R=A*math.cos(2*pi*t/lamda)/q
-				I=A*math.sin(2*pi*t/lamda)/q	
-				RPAT=RPAT+R
 				IPAT=IPAT+I
 		print RPAT*RPAT+IPAT*IPAT
 
