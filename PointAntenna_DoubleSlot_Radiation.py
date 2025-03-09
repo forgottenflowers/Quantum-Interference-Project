8d2696037cb2dd7f5042de122f94970ea555811b
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-03-09 10:41:48 +0100

    Update PointAntenna_DoubleSlot_Radiation.py

--- PointAntenna_DoubleSlot_Radiation.py
+++ PointAntenna_DoubleSlot_Radiation.py
@@ -1,22 +1,21 @@
 # Generate radiation pattern, when point antenna radiation is blocked by a double slot (reverse of double-slit).
 
-point_double slot.txt
 import math
 
 def run():
-	lamda=5.0
-	l=30.0	#even
-	b=12.0
-	d=100.0
-	D=1000000.0
+	lamda=5.0       			# Wavelength
+	l=30.0					# length of slit (even)
+	b=12.0					# breadth of slit (even)
+	d=100.0					# distance between slits
+	D=1000000.0				# distance of detection from screen
 	F=(b*b)/(D*lamda)
 	print "Fresnel number = ",F,":"
-	I0=10.0
+	I0=10.0         			# Intensity amplitude
 	A=math.sqrt(I0)
-	p=1000000
+	p=1000000      				# number of data points
 	pi=3.1416
-	size=1000000
-	ant=500
+	size=1000000				# size of open space we are calculating for
+	ant=500					# distance of antenna from screen
 
 	for np in range(-p,p+1):
 		RPAT=0
@@ -29,16 +28,14 @@ def run():
 
 					if (nb>=d/2) & (nb<=b+d/2):
 						continue		
-				y=np-nb			
-				temp=nl*nl+D*D
-				add=math.sqrt(nb*nb+nl*nl+ant*ant)
-				root=math.sqrt(y*y+temp)
-				t=root+add
-				q=root*add				
-				R=A*math.cos(2*pi*t/lamda)/q
-				I=A*math.sin(2*pi*t/lamda)/q	
-				RPAT=RPAT+R
+				y=np-nb                              #vertical displacement y			
+				temp=nl*nl+D*D                       #x^2+z^2
+				add=math.sqrt(nb*nb+nl*nl+ant*ant)   #antenna to screen
+				root=math.sqrt(y*y+temp)             #screen to detector
+				t=root+add                           #total path length 
+				q=root*add                           #product (intensity to fall)				
+				R=A*math.cos(2*pi*t/lamda)/q         #real
+				I=A*math.sin(2*pi*t/lamda)/q         #imaginary	
+				RPAT=RPAT+R                          #superposition
 				IPAT=IPAT+I
 		print RPAT*RPAT+IPAT*IPAT
-
-
