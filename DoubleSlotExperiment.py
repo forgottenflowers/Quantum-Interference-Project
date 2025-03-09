ecda35a409de51f1652ac1702222f2860b684b77
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-03-09 12:59:54 +0100

    Create DoubleSlotExperiment.py

---
+++ DoubleSlotExperiment.py
@@ -0,0 +1,40 @@
+import math
+
+def run():
+	lamda=5.0
+	l=30.0	#even
+	b=12.0
+	D=60.0
+	F=(b*b)/(D*lamda)
+	print "Fresnel number = ",F,":"
+	I0=10.0
+	A=math.sqrt(I0)
+	p=50
+	pi=3.1416
+	size=100
+	ant=120
+	d=18.0
+
+	for np in range(-p,p):
+		RPAT=0
+		IPAT=0
+		for nl in range(-size,size+1):
+			for nb in range(-size,size+1):
+				if (nl>=-l/2) & (nl<=l/2):
+					if (nb>=np-d-3*b/2) & (nb<=np-d-b/2):
+						continue
+
+					if (nb>=np+d+b/2) & (nb<=np+d+3*b/2):
+						continue					
+				y=np-nb			
+				temp=nl*nl+D*D
+				add=math.sqrt(nb*nb+nl*nl+ant*ant)
+				root=math.sqrt(y*y+temp)
+				t=root+add
+				q=root*add				
+				R=A*math.cos(2*pi*t/lamda)/q
+				I=A*math.sin(2*pi*t/lamda)/q	
+				RPAT=RPAT+R
+				IPAT=IPAT+I
+		print RPAT*RPAT+IPAT*IPAT
+
